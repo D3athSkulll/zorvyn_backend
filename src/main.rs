4a5f02d8b7115ed736c8b36bef1c6c44cc8b1904
Shivam Deolankar <shivam.deolankar@gmail.com>
2026-04-04 07:42:30 +0530

    Fix minor issue

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ async fn main(){
     let app = Router::new()
         .route("/", get(root));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1::3000")
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
 
-    println!("Server running on http:/127.0.0.1::3000");
+    println!("Server running on http://127.0.0.1::3000");
 
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn root()-> &'static str{
-    "Backend is running !!"
+    "Backend is running !"
 }
