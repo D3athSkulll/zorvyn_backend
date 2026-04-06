96b209e625c6cc708711ab97fcafa110ab80eec0
Shivam Deolankar <shivam.deolankar@gmail.com>
2026-04-06 14:26:41 +0530

    feat: rbac for transaction routes properly

--- src/middlewares/role.rs
+++ src/middlewares/role.rs
@@ -4,24 +4,37 @@ use axum::{
     middleware::Next,
     response::Response,
 };
-use crate::utils::jwt::Claims;
+use serde_json::json;
 
-pub fn require_role(required_role: &'static str)
-    -> impl Fn(Request, Next) -> std::pin::Pin<Box<dyn std::future::Future<Output = Result<Response, StatusCode>> + Send>>
+use crate::utils::{
+    jwt::Claims,
+    app_error::AppError
+
+};
+
+pub fn require_roles(allowed_roles: Vec<&'static str>,)
+    -> impl Fn(Request, Next)
+        -> std::pin::Pin<Box<dyn std::future::Future<Output = Result<Response, AppError>> + Send>>
     + Clone{
         move |req: Request, next: Next| {
-            let required_role = required_role.to_string();
+            let allowed_roles = allowed_roles.clone();
 
             Box::pin(async move{
                 let claims = req.extensions().get::<Claims>();
 
                 if let Some(claims) = claims{
-                    if claims.role == required_role{
+                    if allowed_roles.contains(&claims.role.as_str()){
                         return Ok(next.run(req).await);
                     }
                 }
 
-                Err(StatusCode::FORBIDDEN)
+                Err(AppError{
+                    status: StatusCode::FORBIDDEN,
+                    body: json!({
+                        "success": false,
+                        "messsage": "Access Denied"
+                    })
+                })
             })
         }
     }
--- src/routes/transaction_routes.rs
+++ src/routes/transaction_routes.rs
@@ -2,13 +2,31 @@ use axum::{Router, routing::{post, get, put, delete}, middleware};
 use crate::{
     config::state::AppState,
     handlers::transaction_handler::{create_tx, get_transactions, update_tx, delete_tx},
-    middlewares::auth::auth_middleware
+    middlewares::{
+        auth::auth_middleware,
+        role::require_roles
+    }
 };
 
 pub fn transaction_routes()-> Router<AppState>{
-    Router::new()
-        .route("/transactions", post(create_tx).get(get_transactions))
-        .route("/transactions/{id}", put(update_tx))
-        .route("/transactions/{id}", delete(delete_tx))
+    // Viewer (GET only)
+    let viewer_routes = Router::new()
+        .route("/transactions", get(get_transactions))
+        .layer(middleware::from_fn(require_roles(vec!["viewer", "analyst", "admin"])));
+
+    // Analyst (POST + PUT)
+    let analyst_routes = Router::new()
+        .route("/transactions", post(create_tx))
+        .route("/transactions/:id", put(update_tx))
+        .layer(middleware::from_fn(require_roles(vec!["analyst", "admin"])));
+
+    // Admin (DELETE only)
+    let admin_routes = Router::new()
+        .route("/transactions/:id", delete(delete_tx))
+        .layer(middleware::from_fn(require_roles(vec!["admin"])));
+
+    viewer_routes
+        .merge(analyst_routes)
+        .merge(admin_routes)
         .layer(middleware::from_fn(auth_middleware))
 }
